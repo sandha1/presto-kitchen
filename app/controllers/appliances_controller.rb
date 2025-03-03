b9bef72f27b14714fa8d2b98ce0ae47632243ff5
Laura Mendelzon <laura.mendelzon@gmail.com>
2025-03-03 16:47:15 +0100

    Add route, controller action and view for appliances#show

--- app/controllers/appliances_controller.rb
+++ app/controllers/appliances_controller.rb
@@ -1,2 +1,7 @@
 class AppliancesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def show
+    @appliance = Appliance.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
   #     patch 'accept', to: 'bookings#accept'
   #   end
   # end
+  resources :appliances, only: [:show]
 end
