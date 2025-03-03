a7f3d55c8cd0ec6ff7eba5b7a891e186ddea08fd
Carine Czodor <caamlo@hotmail.com>
2025-03-03 16:48:54 +0100

    started new et create

--- app/controllers/appliances_controller.rb
+++ app/controllers/appliances_controller.rb
@@ -1,2 +1,20 @@
 class AppliancesController < ApplicationController
+
+  def new
+    @appliance = Appliances.new
+  end
+
+  def create
+    @appliance = Appliance.new(appliance_params)
+    if @appliance.save
+      redirect_to appliance_path(@appliance)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def appliance_params
+    params.require(:appliance).permit(:name, :description, :price)
+  end
+
 end
