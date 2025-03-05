6823d86294c37fc37368b1061a02d12f5c131d7f
Carine Czodor <caamlo@hotmail.com>
2025-03-05 12:21:59 +0100

    index controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,15 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = @user.booking
+  end
+
+
+  private
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+
   def new
     @booking = Booking.new
   end
