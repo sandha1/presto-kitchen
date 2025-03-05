bec5c7b78c3608136a5e521d7a1956fe73821be6
Carine Czodor <caamlo@hotmail.com>
2025-03-05 11:44:32 +0100

    index de booking controller et vue, non testé

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,13 @@
 class BookingsController < ApplicationController
 
-  
+  def index
+    @bookings = @user.booking
+  end
+
+
+  private
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
 end
