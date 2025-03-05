e7e07aec9a5077c1c20fe0eb758d311ee39cde16
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-03-05 14:10:26 +0100

    Added action et view de My appliances

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+
   def home
+  end
+
+  def my_appliances
+    @appliances = Appliance.where(user: current_user)
   end
 end
